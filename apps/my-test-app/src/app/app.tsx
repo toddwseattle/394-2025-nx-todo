f6c01c4255c00a546ce70ce4ea75146bb69ce532
Todd Warren <todd@warrenfamily.org>
2025-05-05 21:02:17 -0500

    basic app.tsx and styling

--- apps/my-test-app/src/app/app.tsx
+++ apps/my-test-app/src/app/app.tsx
@@ -1,11 +1,32 @@
 // Uncomment this line to use CSS modules
-// import styles from './app.module.css';
+import styles from './app.module.css';
 import NxWelcome from './nx-welcome';
+import { useState } from 'react';
 
 export function App() {
+  const [showAddTodo, setShowAddTodo] = useState(false);
+
   return (
-    <div>
-      <NxWelcome title="my-test-app" />
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <h1>My Todo List</h1>
+      </header>
+
+      <main className={styles.todoList}>
+        {/* Todo list component will go here */}
+        <div>Todo items will appear here</div>
+      </main>
+
+      <button className={styles.addButton} onClick={() => setShowAddTodo(true)}>
+        +
+      </button>
+
+      {showAddTodo && (
+        <div>
+          {/* Add todo modal component will go here */}
+          <button onClick={() => setShowAddTodo(false)}>Close</button>
+        </div>
+      )}
     </div>
   );
 }
